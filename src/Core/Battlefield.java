f6592418f1d79d873ea572c4a2b74a66f51dff3f
ntriquenaux <ntriquenaux@MOI.(none)>
2012-04-15 18:56:39 +0200

    field/Battlefield reflections

--- src/Core/Battlefield.java
+++ src/Core/Battlefield.java
@@ -1,5 +1,15 @@
 package Core;
 
-public class Battlefield {
+import java.util.ArrayList;
+
+public class Battlefield 
+{
+	private ArrayList<Field> battlefieldZones;
+	
+	
+	public Battlefield()
+	{
+
+	}
 
 }
--- src/Core/Field.java
+++ src/Core/Field.java
@@ -5,10 +5,17 @@ import java.util.ArrayList;
 public class Field 
 {
 	private ArrayList<Troop> stationatedTroops;
-	private int playerOwner;
-	private String position;
+	private int playerOwner; //0 = player 0; 1 = player 1; -1 = both
+	private String position; // RM, RR, RL, FM, FR, FL,EZ;
 	
 	public Field()
+	{
+		
+	}
+	
+	
+	//update thee Troopd stationated on the field
+	public void udpate()
 	{
 		
 	}
