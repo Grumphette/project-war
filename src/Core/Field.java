dac0ade592bf870454f363eebfbf3993143b900a
nicolas.triquenaux <nicolas.triquenaux@gmail.com>
2012-04-19 15:20:46 +0000

    Field is in fact a generic type of field, that can be crossable or not, give bonuses or movement penalty

--- src/Core/Field.java
+++ src/Core/Field.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import misc.Coord2D;
 
 
-public class Field 
+public abstact class Field 
 {
-	private Troop stationatedTroops;
+  private bool crossable;
+  private int movementPenalty;
+  private Ability grantedBonus;
 	
 	public Field()
 	{
@@ -15,9 +17,18 @@ public class Field
 	}
 	
 	
-	//update thee Troopd stationated on the field
-	public void udpate()
-	{
-		
-	}
+	public void isCrossable()
+  {
+    return crossable;
+  }
+  
+  public Ability getGrantedBonus()
+  {
+    return grantedBonus;
+  }
+  
+  public int getMovementPenalty()
+  {
+     return movementPenalty;
+  }
 }
