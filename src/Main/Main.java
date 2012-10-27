b1f20b61b3595747f086c52c82b78b332b94a3d0
nicolas <ntriquenaux@ptiMoi.(none)>
2012-10-27 14:39:56 +0200

    adding the Doxyfile for further documentation

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,13 +6,15 @@ import misc.GuiGeneralConfig;
 import Core.CoreGame;
 import DesktopGUI.MainWindow;
 
-/** Special fonts have to be installed in  the ~/.fonts */
-
+// Special fonts have to be installed in  the ~/.fonts
 
 public class Main {
 
 	
-	@SuppressWarnings("unused")
+	/**
+	 * @brief Entry point for the application, start the Gui and the game behind
+	 * @args args Application argument - not used
+	 * */
 	public static void main(String[] args) 
 	{
 		//Starting the Game core, that will handle the game
