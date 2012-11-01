a8accb3667fd86c6c691d8c512c4bc817f83dee3
nicolas <ntriquenaux@ptiMoi.(none)>
2012-11-01 17:30:55 +0100

    work on the race selection interface has started

--- src/DesktopGUI/RaceSelection.java
+++ src/DesktopGUI/RaceSelection.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import Core.CoreGame;
 import GUIobjects.BackImage;
@@ -16,10 +19,17 @@ import misc.GuiGeneralConfig;
 @SuppressWarnings("serial")
 public class RaceSelection extends JPanel
 {
+	private JLabel chooseUrArmyLbl;
 	private JPanel verticalLayoutPnl;
 	private JPanel horizontalLayoutPnl;
+	private JPanel armyRacesPnlBack;
+	private JLabel armyDescriptionLbl;
 	private JPanel armyLayout;
 	private JPanel btnLayoutPnl;
+	
+	private JScrollPane armyRacesScrollPnl;
+	
+	private ArrayList<JPanel> allArmyPnl;
 	private ArrayList<String> racesAvailable;
 	private int state;
 	
@@ -33,12 +43,41 @@ public class RaceSelection extends JPanel
 		CoreGeneralConfig coreConfig = CoreGeneralConfig.getCoreConfigSingleton();
 		GuiGeneralConfig guiConfig = GuiGeneralConfig.getGuiConfigSingleton();
 		
-		racesAvailable = coreConfig.getarmyRaces();
+		
+		chooseUrArmyLbl = new JLabel("Choose your army race");
+		
+		
+		armyRacesPnlBack = new JPanel();
+		armyRacesPnlBack.setLayout(new BoxLayout(armyRacesPnlBack, BoxLayout.X_AXIS));
 		for(String tmp : racesAvailable)
 		{
-			//BackImage tmpIcons = guiConfig.getGeneralImage(tmp);
+			BackImage tmpIcons = guiConfig.getGeneralImage(tmp);
+			
 			//Logo = new JPanelWithImg(LogImgInfo.getFinalImage());
 		}
+		
+		
+		
+		
+		
+		
+		armyRacesScrollPnl = new JScrollPane();
+		
+		
+		horizontalLayoutPnl = new JPanel();
+		horizontalLayoutPnl.setLayout(new BoxLayout(horizontalLayoutPnl, BoxLayout.X_AXIS));
+		
+		
+		racesAvailable = coreConfig.getarmyRaces();
+		
+		
+		verticalLayoutPnl = new JPanel();
+		verticalLayoutPnl.setLayout(new BoxLayout(verticalLayoutPnl, BoxLayout.Y_AXIS));
+		verticalLayoutPnl.add(chooseUrArmyLbl);
+		
+		this.add(verticalLayoutPnl);
+		
+		
 	}
 	
 	public int getState()
